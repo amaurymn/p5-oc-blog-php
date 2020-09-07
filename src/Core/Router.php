c0ee68464e6728eb92934c2f9a47e731448ef80f
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-09-07 20:52:01 +0200

    Ajout de zones de controllers

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -2,7 +2,7 @@
 
 namespace App\Core;
 
-use App\Controllers\HomeController;
+use App\Controllers\PublicController\HomeController;
 use Symfony\Component\Yaml\Yaml;
 
 class Router
@@ -19,7 +19,7 @@ class Router
 
         foreach ($routes as $route) {
             if (preg_match('#^'. $route['uri'] .'$#', $_SERVER['REQUEST_URI'], $matches)) {
-                $controller = '\\App\\Controllers\\' . $route['controller'];
+                $controller = '\\App\\Controllers\\' . $route['area'] .'Controller\\'. $route['controller'];
                 $params = array_combine($route['parameters'], array_slice($matches,1));
 
                 return $this->controller = new $controller($route['action'], $params);
