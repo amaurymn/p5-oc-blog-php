26dcbe02585e17db99b62fa4ca4518f778e72189
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-09-12 20:37:55 +0200

    Add @ for templates

--- src/Controllers/PublicController/HomeController.php
+++ src/Controllers/PublicController/HomeController.php
@@ -9,16 +9,11 @@ class HomeController extends Controller
 
     public function executeShowHome()
     {
-        $this->render('public/homepage.twig');
+        $this->render('@public/homepage.html.twig');
     }
 
     public function executeError404()
     {
-        $this->render('public/404.twig');
-    }
-
-    public function executeTest()
-    {
-        $this->render('public/test.twig', ['params' => $this->params['param']]);
+        $this->render('@public/404.html.twig');
     }
 }
