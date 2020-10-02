595c66a8a4ee8dfed0f1ef422a07a79469399a63
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-10-02 21:58:22 +0200

    Add Article Manager

--- src/Manager/ArticleManager.php
+++ src/Manager/ArticleManager.php
@@ -1,14 +1,13 @@
 <?php
 
-
 namespace App\Manager;
-
 
 use App\Core\Manager;
 
 class ArticleManager extends Manager
 {
-    public function countArticlesby() {
-
+    public function setTable(string $tableName)
+    {
+        $this->table = $tableName;
     }
 }
