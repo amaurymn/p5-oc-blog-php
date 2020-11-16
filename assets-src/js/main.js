e0d9d55b2ac9760ac846343f92c5b2d5b9c5e871
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-11-16 09:59:38 +0100

    Review: Remove Autoclose

--- assets-src/js/main.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    'use strict';
-    window.setTimeout(function () {
-        $('.autoclose').fadeTo(1000, 0).slideUp(1000, function () {
-            $(this).remove();
-        });
-    }, 10000);
-})();
