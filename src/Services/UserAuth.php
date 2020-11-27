6d09e62fbf478c7c91b20614bc707f03703ef76c
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-11-27 22:10:14 +0100

    Add User Auth service

---
+++ src/Services/UserAuth.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use App\Manager\UserManager;
+
+class UserAuth
+{
+
+    private $flash;
+    private $session;
+
+    public function __construct()
+    {
+        $this->flash   = new FlashBag();
+        $this->session = new Session();
+    }
+
+    /**
+     * @param array $post
+     * @return bool
+     */
+    public function authenticateUser(array $post): bool
+    {
+        $user = (new UserManager())->getUser($post['email']);
+
+        if ($user && password_verify($post['password'], $user['password'])) {
+            $this->session->set([
+                'auth'     => true,
+                'id'       => $user['id'],
+                'username' => $user['username'],
+                'role'     => $user['role']
+            ]);
+
+            return true;
+        }
+
+        $this->flash->set(FlashBag::ERROR, 'Utilisateur ou mot de passe incorrect.');
+
+        return false;
+    }
+}
