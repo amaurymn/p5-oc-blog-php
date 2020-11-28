27f0cb9ff8bf2d77b3daa7824faae25a6d390569
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-11-28 03:41:01 +0100

    Remove unused method

--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -3,27 +3,7 @@
 namespace App\Manager;
 
 use App\Core\Manager;
-use PDO;
 
 class UserManager extends Manager
 {
-    /**
-     * @param string $email
-     * @return mixed
-     */
-    public function getUser(string $email)
-    {
-        $stmt = $this->pdo->prepare("
-            SELECT *
-            FROM user AS u
-                LEFT JOIN admin AS ad
-                ON u.id = ad.user_id 
-            WHERE u.email = :email
-        ");
-
-        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
-        $stmt->execute();
-
-        return $stmt->fetch();
-    }
 }
