a1956e3defed5a37ebc4890ddfd12518e5420fd4
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-11-28 04:03:53 +0100

    Add method to check if user already exist

--- src/Services/UserAuth.php
+++ src/Services/UserAuth.php
@@ -26,15 +26,33 @@ class UserAuth
 
         if ($user && password_verify($post['password'], $user['password'])) {
             $this->session->set([
-                'auth'     => true,
-                'id'       => $user['id'],
-                'role'     => $user['role']
+                'auth' => true,
+                'id'   => $user['id'],
+                'role' => $user['role']
             ]);
 
             return true;
         }
 
         $this->flash->set(FlashBag::ERROR, 'Utilisateur ou mot de passe incorrect.');
+
+        return false;
+    }
+
+    /**
+     * @param array $post
+     * @return bool
+     */
+    public function isUserAlreadyRegistered(array $post): bool
+    {
+        $user = (new UserManager())->findOneBy(['email' => $post['email']]);
+
+        if ($user) {
+            $this->flash->set(FlashBag::ERROR, 'Cette adresse email est déjà utilisée.');
+
+            return true;
+        }
+
         return false;
     }
 }
