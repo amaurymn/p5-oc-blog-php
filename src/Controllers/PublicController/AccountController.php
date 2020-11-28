db9195ac0f5455c36e411b92258afceb3cdeb414
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-11-28 03:48:51 +0100

    Update methods

--- src/Controllers/PublicController/AccountController.php
+++ src/Controllers/PublicController/AccountController.php
@@ -3,12 +3,28 @@
 namespace App\Controllers\PublicController;
 
 use App\Core\Controller;
+use App\Core\Validator;
+use App\Entity\Admin;
+use App\Entity\User;
 use App\Exception\TwigException;
+use App\Manager\UserManager;
+use App\Services\FlashBag;
 use App\Services\UserAuth;
 use App\Services\Session;
 
 class AccountController extends Controller
 {
+    /** @var UserManager */
+    private UserManager $manager;
+    private FlashBag $flashBag;
+
+    public function __construct($action, $params)
+    {
+        parent::__construct($action, $params);
+        $this->manager  = new UserManager();
+        $this->flashBag = new FlashBag();
+    }
+
     /**
      * @throws TwigException
      */
@@ -27,10 +43,28 @@ class AccountController extends Controller
 
     /**
      * @throws TwigException
+     * @throws \ReflectionException
      */
     public function executeShowRegister(): void
     {
-        $this->render('@public/register.html.twig');
+        if ($this->isFormSubmit('register')) {
+            $formCheck = (new Validator($_POST));
+            $hadAdmin  = $this->manager->findOneBy(['role' => 'admin']);
+
+            if ($formCheck->registerValidation()) {
+                $user = new User();
+                $hadAdmin ? $user->setRole('user') : $user->setRole('admin');
+
+                $user->hydrate($_POST);
+                $this->manager->create($user);
+
+                $this->flashBag->set(FlashBag::SUCCESS, "Utilisateur crée.");
+                $this->redirectUrl('/login');
+            }
+        }
+        $this->render('@public/register.html.twig', [
+            'formpost' => $_POST
+        ]);
     }
 
     public function executeLogout(): void
