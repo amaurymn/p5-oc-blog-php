3881273e6fb188db00890ea898be4f0123c5806d
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-11-30 23:56:29 +0100

    update account controller methods

--- src/Controllers/PublicController/AccountController.php
+++ src/Controllers/PublicController/AccountController.php
@@ -4,19 +4,24 @@ namespace App\Controllers\PublicController;
 
 use App\Core\Controller;
 use App\Core\Validator;
+use App\Entity\Admin;
 use App\Entity\User;
-use App\Exception\EntityNotFoundException;
 use App\Exception\TwigException;
+use App\Manager\AdminManager;
 use App\Manager\UserManager;
 use App\Services\FlashBag;
-use App\Services\UserAuth;
 use App\Services\Session;
+use App\Services\UserAuth;
 
 class AccountController extends Controller
 {
+    const ROLE_ADMIN = 'admin';
+    const ROLE_USER  = 'user';
+
     private UserManager $manager;
     private FlashBag $flashBag;
     private UserAuth $userAuth;
+    private Session $session;
 
     public function __construct($action, $params)
     {
@@ -24,6 +29,7 @@ class AccountController extends Controller
         $this->manager  = new UserManager();
         $this->flashBag = new FlashBag();
         $this->userAuth = new UserAuth();
+        $this->session  = new Session();
     }
 
     /**
@@ -40,21 +46,26 @@ class AccountController extends Controller
 
     /**
      * @throws TwigException
-     * @throws EntityNotFoundException
      * @throws \ReflectionException
      */
     public function executeShowRegister(): void
     {
         if ($this->isFormSubmit('register')) {
-            $formCheck = (new Validator($_POST));
-            $hadAdmin  = $this->manager->findOneBy(['role' => 'admin']);
+            $formCheck         = (new Validator($_POST));
+            $adminAlreadyExist = $this->userAuth->isAdminAlreadyExist();
 
             if ($formCheck->registerValidation() && !$this->userAuth->isUserAlreadyRegistered($_POST)) {
                 $user = new User();
-                $hadAdmin ? $user->setRole('user') : $user->setRole('admin');
 
+                $role = (!$adminAlreadyExist) ? self::ROLE_ADMIN : self::ROLE_USER;
+                $user->setRole($role);
                 $user->hydrate($_POST);
+
                 $this->manager->create($user);
+
+                if (!$adminAlreadyExist) {
+                    $this->createAdmin($user);
+                }
 
                 $this->flashBag->set(FlashBag::SUCCESS, "Utilisateur crée.");
                 $this->redirectUrl('/login');
@@ -63,6 +74,23 @@ class AccountController extends Controller
         $this->render('@public/register.html.twig', [
             'formpost' => $_POST
         ]);
+    }
+
+    /**
+     * @param User $user
+     * @throws \ReflectionException
+     */
+    public function createAdmin(User $user): void
+    {
+        $adminManager = new AdminManager();
+        $admin        = new Admin();
+
+        $getUser = $this->manager->findBy(['email' => $user->getEmail()]);
+        $admin->setUserId($getUser['id']);
+        $adminManager->create($admin);
+
+        $this->flashBag->set(FlashBag::SUCCESS, "Admin crée.");
+        $this->redirectUrl('/login');
     }
 
     public function executeLogout(): void
