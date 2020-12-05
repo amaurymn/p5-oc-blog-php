f74b07bf51f229575b6c7769a1b9962a2ca7489a
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-12-05 03:32:08 +0100

    Add comment entity

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -10,6 +10,9 @@ class Comment extends Entity
     use TimestampableEntity;
 
     private string $content;
+    private int $online;
+    private int $articleId;
+    private int $userId;
 
     /**
      * @return string
@@ -21,9 +24,69 @@ class Comment extends Entity
 
     /**
      * @param string $content
+     * @return $this
      */
-    public function setContent(string $content): void
+    public function setContent(string $content): Comment
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOnline(): int
+    {
+        return $this->online;
+    }
+
+    /**
+     * @param int $online
+     * @return $this
+     */
+    public function setOnline(int $online): Comment
+    {
+        $this->online = $online;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getArticleId(): int
+    {
+        return $this->articleId;
+    }
+
+    /**
+     * @param $articleId
+     * @return $this
+     */
+    public function setArticleId($articleId): Comment
+    {
+        $this->articleId = $articleId;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param int $userId
+     * @return $this
+     */
+    public function setUserId(int $userId): Comment
+    {
+        $this->userId = $userId;
+
+        return $this;
     }
 }
