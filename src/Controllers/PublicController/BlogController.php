c1468dc3ba8702edfdbb233c5bfc194c2539e92f
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-12-05 03:31:30 +0100

    Add comments controller

--- src/Controllers/PublicController/BlogController.php
+++ src/Controllers/PublicController/BlogController.php
@@ -3,35 +3,84 @@
 namespace App\Controllers\PublicController;
 
 use App\Core\Controller;
-
+use App\Core\Validator;
+use App\Entity\Comment;
+use App\Exception\EntityNotFoundException;
+use App\Exception\TwigException;
 use App\Manager\ArticleManager;
+use App\Manager\CommentManager;
+use App\Services\FlashBag;
+use App\Services\Session;
+use ReflectionException;
 
 class BlogController extends Controller
 {
-    private $manager;
+    private ArticleManager $articleManager;
+    private CommentManager $commentManager;
+    private Session $session;
+    private FlashBag $flashBag;
 
     public function __construct($action, $params)
     {
         parent::__construct($action, $params);
-        $this->manager = new ArticleManager();
+        $this->articleManager = new ArticleManager();
+        $this->commentManager = new CommentManager();
+        $this->session        = new Session();
+        $this->flashBag       = new FlashBag();
     }
 
-    public function executeShowBlog()
+    /**
+     * @throws TwigException
+     */
+    public function executeShowBlog(): void
     {
-        $articles = $this->manager->findAll(['created_at' => 'DESC'], 3);
+        $articles = $this->articleManager->findAll(['created_at' => 'DESC'], 3);
 
         $this->render('@public/blog.html.twig', [
             'articles' => $articles
         ]);
     }
 
-    public function executeShowBlogSingle()
+    /**
+     * @throws EntityNotFoundException
+     * @throws ReflectionException
+     * @throws TwigException
+     */
+    public function executeShowSingle(): void
     {
+        $article  = $this->articleManager->findOneBy(['slug' => $this->params['slug']]);
+        $comments = $this->commentManager->getCommentsFromArticle($article->getId(), true);
 
-        $article = $this->manager->findOneBy(['slug' => $this->params['slug']]);
+        if ($this->isFormSubmit('publish') && (new Validator($_POST))->commentValidation()) {
+            $this->addNewComment($article->getId(), $_POST);
+        }
 
         $this->render('@public/blogSingle.html.twig', [
-            'article' => $article
+            'article'  => $article,
+            'comments' => $comments,
+            'post'     => $_POST
         ]);
+    }
+
+    /**
+     * @param int $articleId
+     * @param array $post
+     * @throws ReflectionException
+     */
+    private function addNewComment(int $articleId, array $post): void
+    {
+        $manager = new CommentManager();
+        $comment = new Comment();
+
+        $comment
+            ->setContent($post['content'])
+            ->setArticleId($articleId)
+            ->setUserId($this->session->get('id'))
+            ->setOnline(1);
+        $comment->hydrate($_POST);
+        $manager->create($comment);
+
+        $this->flashBag->set(FlashBag::SUCCESS, "Commentaire ajouté.");
+        $this->redirectUrl($_SERVER['REQUEST_URI']);
     }
 }
