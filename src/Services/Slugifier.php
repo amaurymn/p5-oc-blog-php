8d2a081b98690e99bd15b10ec29de436ddf17299
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-12-08 19:46:54 +0100

    Fix: slugifier

--- src/Services/Slugifier.php
+++ src/Services/Slugifier.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Manager\ArticleManager;
+
 class Slugifier
 {
     public function __construct()
@@ -17,31 +19,41 @@ class Slugifier
      */
     public function slugify($rawString, $delimiter = '-', $threshold = 2)
     {
-
         if (!in_array($delimiter, ['-', '_'])) {
             return false;
         }
         // remove accents
         $cleanString = iconv('UTF-8', 'ASCII//TRANSLIT', $rawString);
-
         // replace underscores and slash by a dash
         $cleanString = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", $delimiter, $cleanString);
-
         // Replace unwanted chars by a dash and space
         $cleanString = preg_replace("/[^a-zA-Z0-9 -]/", '', $cleanString);
-
         // Add threshold for little words
         $cleanString = preg_replace("/\b[a-zA-Z0-9]{1,$threshold}\b/", $delimiter, $cleanString);
-
         // Replace spaces and doublons $delimiter by one $delimiter
         $cleanString = preg_replace("/[\/_|+ -]+/", $delimiter, $cleanString);
-
         // Set string in lowercase
         $cleanString = strtolower($cleanString);
-
         // Trim possible delimiter at start and end of string
         $cleanString = trim($cleanString, $delimiter);
 
         return $cleanString;
+    }
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    public function getUniqueSlug(string $string): string
+    {
+        $slug  = $this->slugify($string);
+        $count = (new ArticleManager())->checkSlugExist($slug);
+
+        if ($count > 0) {
+            ++$count;
+            $slug .= "-" . $count;
+        }
+
+        return $slug;
     }
 }
