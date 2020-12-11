44f3a2c9190831d91442c86a96b0469856a3e778
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-12-11 15:59:27 +0100

    Fix: codeclimate warning Add: new query

--- src/Manager/CommentManager.php
+++ src/Manager/CommentManager.php
@@ -9,27 +9,40 @@ class CommentManager extends Manager
 {
     /**
      * @param int $articleId
-     * @param bool $online
      * @return array
      */
-    public function getCommentsFromArticle(int $articleId, bool $online = true): array
+    public function getCommentsFromArticle(int $articleId): array
     {
         $query = "
             SELECT c.id, c.content, c.online, c.created_at, u.user_name
             FROM comment AS c
             LEFT JOIN user AS u
-            ON u.id = c.user_id
+                ON c.user_id = u.id
             WHERE c.article_id = :articleId
+            AND c.online = 1
+            ORDER BY c.created_at DESC
         ";
-
-        if ($online) {
-            $query .= " AND c.online = 1";
-        }
-
-        $query .= " ORDER BY created_at DESC";
 
         $stmt = $this->pdo->prepare($query);
         $stmt->bindValue(':articleId', $articleId, PDO::PARAM_INT);
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+    }
+
+    public function getCommentAndAuthor()
+    {
+        $query = "
+            SELECT c.id, c.content, c.online, c.created_at, u.user_name, a.title AS artTitle, a.slug AS artSlug
+            FROM comment AS c
+            LEFT JOIN user AS u
+                ON c.user_id = u.id
+            LEFT JOIN article AS a
+                ON c.article_id = a.id
+            ORDER BY c.created_at DESC
+        ";
+
+        $stmt = $this->pdo->prepare($query);
         $stmt->execute();
 
         return $stmt->fetchAll();
