3561099ed313bb79b1c1ef49522e5d2bf6a2473e
amaurymn <53151363+amaurymn@users.noreply.github.com>
2020-12-29 23:20:46 +0100

    Add dashboard stats

--- src/Controllers/AdminController/DashboardController.php
+++ src/Controllers/AdminController/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Controllers\AdminController;
 
 use App\Core\Controller;
 use App\Exception\TwigException;
+use App\Manager\CommentManager;
+use App\Services\DashboardStats;
 use App\Services\Session;
 
 class DashboardController extends Controller
@@ -28,6 +30,17 @@ class DashboardController extends Controller
             $this->session->redirectUrl('/dashboard/profil');
         }
 
-        $this->render('@admin/dashboard.html.twig');
+        $stats    = new DashboardStats();
+        $comments = new CommentManager();
+
+        $count = $stats->getStats();
+
+        $this->render('@admin/dashboard.html.twig', [
+            'count_onl_articles' => $count['art_online'],
+            'count_com_total'    => $count['com_total'],
+            'count_com_pending'  => $count['com_pending'],
+            'count_reg_users'    => $count['usr_registered'],
+            'lastComments'       => $comments->getCommentAndAuthor(3)
+        ]);
     }
 }
